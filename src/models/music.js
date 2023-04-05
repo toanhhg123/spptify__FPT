6289d3dcd70978604775fe39cbd29585147ef75d
toanhhg123 <toanhhg123@gmail.com>
2023-04-05 11:13:28 +0700

    edit music

--- src/models/music.js
+++ src/models/music.js
@@ -1,43 +1,51 @@
 const mongoose = require("mongoose");
 
-const musicSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const musicSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      auto: true,
+    },
+    singer: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    releaseDate: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    audioFile: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
-  artist: {
-    type: String,
-    required: true,
-  },
-  album: {
-    type: String,
-    required: true,
-  },
-  releaseDate: {
-    type: Date,
-    required: true,
-  },
-  duration: {
-    type: Number,
-    required: true,
-  },
-  genre: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  audioFile: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Category",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Music = mongoose.model("Music", musicSchema);
 
