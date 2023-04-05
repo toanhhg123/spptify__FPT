76ae3bbdf569bdca9ec06d7bfaa3f55e5fe5fd8e
toanhhg123 <toanhhg123@gmail.com>
2023-04-05 10:51:45 +0700

    add music and category

--- src/models/music.js
+++ src/models/music.js
@@ -1,10 +1,44 @@
 const mongoose = require("mongoose");
 
 const musicSchema = new mongoose.Schema({
-  _id: {
-    type: mongoose.Types.ObjectId,
-    auto: true,
+  title: {
+    type: String,
+    required: true,
+  },
+  artist: {
+    type: String,
+    required: true,
+  },
+  album: {
+    type: String,
+    required: true,
+  },
+  releaseDate: {
+    type: Date,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  audioFile: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
   },
 });
 
-module.exports = mongoose.model("Music", musicSchema);
+const Music = mongoose.model("Music", musicSchema);
+
+module.exports = Music;
